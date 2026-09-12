12e8a62f43b685b92a884fe981474c4244041f9c
UMRT <59497726+crashdummy88@users.noreply.github.com>
2026-09-12 02:41:53 -0400

    Fix import paths in forum API files

--- functions/api/forum/trending.js
+++ functions/api/forum/trending.js
@@ -1,4 +1,9 @@
-import { json } from '../../_lib/authz.js';
+function json(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: { 'Content-Type': 'application/json; charset=utf-8', 'Cache-Control': 'no-store' },
+  });
+}
 
 export async function onRequestGet(context) {
   const { env, request } = context;
